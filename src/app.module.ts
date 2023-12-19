7176e23b770e738a89ca54b685ae3f4245968c1c
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-12-19 01:26:43 +0100

    Change mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './articles/articles.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), ArticleModule],
+  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017'), ArticleModule],
   controllers: [],
   providers: [],
 })

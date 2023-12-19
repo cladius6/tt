46846deb420b085ea8c2733cbfe7e05cc8f79bcb
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-12-19 02:35:57 +0100

    Refactor add token util class

---
+++ src/utils/token.utils.ts
@@ -0,0 +1,6 @@
+export class TokenUtil {
+  static extractTokenFromHeader(request: Request | Record<string, any>): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}

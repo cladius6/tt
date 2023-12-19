b9f9824135710bc7afd40a8918931a5d7dbc37d6
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-12-19 05:36:36 +0100

    Add redis storage for ThrottlerModule

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,16 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './articles/articles.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis'
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017'), ArticleModule, JwtModule.register({
     global: true,
     secret: 'temp-not-secure-secret',
   }),
-  ThrottlerModule.forRoot([{
-    ttl: 10000,
-    limit: 2,
-  }]),
+  ThrottlerModule.forRoot({
+    throttlers: [{
+      ttl: 10000,
+      limit: 2,
+    }], storage: new ThrottlerStorageRedisService()
+  }),
   ],
 })
 export class AppModule { }

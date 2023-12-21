5a28a1b453d8d35d351c6a8a9d6af37999eb9427
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-12-21 07:31:42 +0100

    Change create dto from zod schema

--- src/modules/articles/dtos/create-article.dto.ts
+++ src/modules/articles/dtos/create-article.dto.ts
@@ -1,6 +1,11 @@
-export class CreateArticleDto {
-  readonly title: string;
-  readonly description?: string;
-  readonly body: string;
-  readonly published?: boolean;
-}
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'nestjs-zod/z';
+
+const createArticleSchema = z.object({
+  title: z.string(),
+  description: z.string().nullable().optional(),
+  body: z.string(),
+  published: z.string().nullable().optional(),
+});
+
+export class CreateArticleDto extends createZodDto(createArticleSchema) {}

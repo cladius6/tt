00a06cbf632ef1fd927b379a1fd887f36649ed91
Cladius <klaudiusz.kowalski@elpassion.pl>
2023-12-21 06:34:14 +0100

    Change remove http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { BaseConfiguration } from './modules/app-config/base-config.service';
-import { HttpExceptionFilter } from './exceptions-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const baseConfig = app.get(BaseConfiguration);
-  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(baseConfig.apiPort);
 }
